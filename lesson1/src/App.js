b73b67a82a5be3ec5367d7c4938aef21b9df2e09
Alex-dqw <alexdqw2016@gmail.com>
2024-04-14 13:39:12 +0300

    react-app-1

---
+++ lesson1/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import User from './pages/user/user';
+
+function App() {
+  return (
+    <div className="App">
+      <User />
+    </div>
+  );
+}
+
+export default App;
